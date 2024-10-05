fd60053b99cebf1afda5c5c92b1af38c35001963
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-10-05 01:37:09 -0700

    Update latest.lua

--- src/latest.lua
+++ src/latest.lua
@@ -18,18 +18,18 @@ local Script = {
 }
 
 --<< Functions >>--
-function Script.Functions.CreateMenuFrame(Location)
+function Script.Functions.CreateClientInfo(Location)
     local Frame = Instance.new("Frame")
-    Frame.Name = "NightlyFrame"
+    Frame.Name = "NightlyInfo"
     Frame.Parent = Location
     Frame.Position = UDim2.new(0.5, -400, 0.5, 300)
     Frame.Size = UDim2.new(1, 0, 0, 80)
-    Frame.BackgroundColor3 = Color3.fromRGB(163, 162, 165)
+    Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
     Frame.BorderColor3 = Color3.fromRGB(27, 42, 53)
     Frame.BorderMode = Enum.BorderMode.Outline
-    Frame.BorderSizePixel = 1
+    Frame.BorderSizePixel = 0
     Frame.LayoutOrder = 2
-    Frame.BackgroundTransparency = 1
+    Frame.BackgroundTransparency = 0.3
     Frame.ZIndex = 3
 
     local UIListLayout = Instance.new("UIListLayout")
@@ -40,8 +40,12 @@ function Script.Functions.CreateMenuFrame(Location)
     UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
     UIListLayout.Padding = UDim.new(0, 12)
 
+    local UICorner = Instance.new("UICorner")
+    UICorner.Parent = Frame
+    UICorner.CornerRadius = UDim.new(0, 10)
+
     return Frame
 end
 
 --<< Initialize >>--
-local ClientInfo = Script.Functions.CreateMenuFrame(Menu)
+local ClientInfo = Script.Functions.CreateClientInfo(Menu)
